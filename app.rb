ed1dd1dd5beeeea9fb616524320e6b04bc9a0591
adielletuchman <adielle.tuchman@mjbha.org>
2018-08-14 17:44:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "Hello, my name is Adielle"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Silver Spring"
+  end 
+  
+  get '/favorite_song' do 
+    "My favorite song is Shnei Mishugaim"
+  end 
+
 end
